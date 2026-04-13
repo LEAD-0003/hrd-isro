031e9418e2e0f2cb7b21516e1c0d33699676a572
0003 <reachawave.bala@gmail.com>
2026-04-13 17:12:59 +0530

    Modify DatabaseSeeder to add new admin user
    Updated user creation logic in DatabaseSeeder.php to include a new admin user with a different password and added timestamps.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,40 +11,55 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         
-        User::create([
-            'name' => 'EMP',
-            'email' => 'emp@isro.gov.in', 
-            'username' => 'emp',
-            'password' => Hash::make('password'),
-            'role' => 'employee',
-            'employee_code' => 'EMP001',
-            'designation' => 'Scientist-SC',
-            'centre' => 'VSSC',
-            'is_active' => 1, 
-        ]);
+        // User::create([
+        //     'name' => 'EMP',
+        //     'email' => 'emp@isro.gov.in', 
+        //     'username' => 'emp',
+        //     'password' => Hash::make('password'),
+        //     'role' => 'employee',
+        //     'employee_code' => 'EMP001',
+        //     'designation' => 'Scientist-SC',
+        //     'centre' => 'VSSC',
+        //     'is_active' => 1, 
+        // ]);
 
      
-        User::create([
-            'name' => 'HRD COORD',
-            'email' => 'coord@isro.gov.in',
-            'username' => 'hrd_coord_002',
-            'password' => Hash::make('password'),
-            'role' => 'coordinator',
-            'designation' => 'HR Manager',
-            'centre' => 'VSSC',
-            'is_active' => 1,
-        ]);
+        // User::create([
+        //     'name' => 'HRD COORD',
+        //     'email' => 'coord@isro.gov.in',
+        //     'username' => 'hrd_coord_002',
+        //     'password' => Hash::make('password'),
+        //     'role' => 'coordinator',
+        //     'designation' => 'HR Manager',
+        //     'centre' => 'VSSC',
+        //     'is_active' => 1,
+        // ]);
 
         
-        User::create([
+        // User::create([
+        //     'name' => 'ISRO HQ ADMIN',
+        //     'email' => 'admin@isro.gov.in',
+        //     'username' => 'hq_admin_01',
+        //     'password' => Hash::make('password'),
+        //     'role' => 'admin',
+        //     'designation' => 'Director',
+        //     'centre' => 'HQ',
+        //     'is_active' => 1,
+        // ]);
+
+         User::create([
+            'id' => 1,
+            'title' => 'Mr.',
             'name' => 'ISRO HQ ADMIN',
             'email' => 'admin@isro.gov.in',
             'username' => 'hq_admin_01',
-            'password' => Hash::make('password'),
+            'password' => Hash::make('password@123'),
             'role' => 'admin',
             'designation' => 'Director',
             'centre' => 'HQ',
             'is_active' => 1,
+            'created_at' => now(),
+            'updated_at' => now(),
         ]);
     }
 }
